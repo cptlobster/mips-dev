452a32e3d3e1b40b9a18884a8d392365d6e784d4
Dustin Thomas <stdio@cptlobster.dev>
2023-09-07 14:07:45 -0500

    fix typescript build

--- src/extension.ts
+++ src/extension.ts
@@ -13,13 +13,19 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('mips-dev.helloWorld', () => {
+	let helloworld = vscode.commands.registerCommand('mips-dev.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from mips-dev!');
 	});
 
-	context.subscriptions.push(disposable);
+	// let spim_repl = vscode.commands.registerCommand('mips-dev.spim.repl', () => {
+	// 	const cp = require('child_process');
+
+	// 	cp.exec(spim.path);
+	// });
+
+	context.subscriptions.push(helloworld);
 }
 
 // This method is called when your extension is deactivated
